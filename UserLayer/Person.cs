63453cffe7849e39346043e71e5f59a2d5da9f14
mort225g <mort225g@edu.campusvejle.dk>
2016-09-28 11:33:08 +0200

    tried to commit again, see if this works

---
+++ UserLayer/Person.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserLayer
+{
+    public abstract class Person
+    {
+        #region Fields
+        private string firstname;
+        private string lastnames;
+        private string ssn;
+
+
+        #endregion
+
+        #region Constructor
+
+        #endregion
+
+        #region Properties
+        public string Firstname
+        {
+            get
+            {
+                return firstname;
+            }
+
+            set
+            {
+                if (String.IsNullOrWhiteSpace(firstname))
+                {
+                    throw new ArgumentException();
+                }
+                firstname = value;
+            }
+        }
+
+        public string Lastnames
+        {
+            get
+            {
+                return lastnames;
+            }
+
+            set
+            {
+                if (String.IsNullOrWhiteSpace(lastnames))
+                {
+                    throw new ArgumentException();
+                }
+                lastnames = value;
+            }
+        }
+
+        protected string Ssn
+        {
+            get
+            {
+                return ssn;
+            }
+
+            set
+            {
+                string error = String.Empty;
+                bool validationSuccess = false;
+
+                try
+                {
+                    validationSuccess = IsSsnValid(value, out error);
+                }
+                catch(Exception)
+                {
+                    throw;
+                }
+                if (!validationSuccess)
+                {
+                    throw new ArgumentException();
+                }
+                else
+                {
+                    ssn = value;
+                }
+                
+            }
+        }
+
+
+        #endregion
+
+        #region Methods
+        public bool IsSsnValid(string ssn, out string error)
+        {
+            
+            bool CorrectlyFormatted = false;
+            int stringCatcher;
+
+            if (String.IsNullOrWhiteSpace(ssn))
+            {
+                error = "something went wrong";
+                throw new ArgumentException();
+            }
+            else if(ssn.Length < 10)
+            {
+                CorrectlyFormatted = false;
+                error = "ssn is too short, please try again";
+            }
+            //currently added, not sure if works
+            else if(!int.TryParse(ssn, out stringCatcher))
+            {
+                CorrectlyFormatted = false;
+                error = "ssn contains letters, please try again";
+            }
+            else
+            {
+                error = string.Empty;
+                CorrectlyFormatted = true;
+            }
+
+
+            return CorrectlyFormatted;
+        }
+
+        
+        #endregion
+
+    }
+}
---
+++ UserLayer/User.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserLayer
+{
+    public class User
+    {
+        #region Fields
+        private string password;
+        private string username;
+
+        #endregion
+
+
+        #region Constructor
+        /// <summary>
+        /// Creates a new instance of this class. Use this class to enable a user to login
+        /// then downcast the object to the appropriate derived type.
+        /// </summary>
+        /// <param name="username">the username (a string)</param>
+        /// <param name="password">the password (a string)</param>
+        public User(string username, string password)
+        {
+            try
+            {
+                Username = username;
+                Password = password;
+            }
+            catch (ArgumentOutOfRangeException) { throw; }
+            catch (ArgumentException) { throw; }
+            
+        }
+        #endregion
+
+
+        #region Properties
+        /// <summary>
+        /// property which encapsulates password
+        /// makes minor validation
+        /// </summary>
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+
+            set
+            {
+                string error = String.Empty;
+                bool validationSuccess = false;
+
+                try
+                {
+                    validationSuccess = IsPasswordValid(value, out error);
+                }
+                catch(Exception)
+                {
+                    throw;
+                }
+                if (!validationSuccess)
+                {
+                    throw new ArgumentException();
+                }
+                else
+                {
+                    password = value;
+                }
+                
+            }
+        }
+
+
+        /// <summary>
+        /// Property which encapsulates Username
+        /// makes minor validation
+        /// </summary>
+        public string Username
+        {
+            get
+            {
+                return username;
+            }
+
+            set
+            {
+                if (String.IsNullOrWhiteSpace(username))
+                {
+                    throw new ArgumentException();
+                }
+                username = value;
+            }
+        }
+
+        #endregion
+
+
+        #region Method
+        
+        public static bool IsPasswordValid(string password ,out string error)
+        {
+            bool CorrectlyFormatted = false;
+
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                error = "something went wrong";
+                throw new ArgumentException();
+            }
+            else if(password.Length < 8)
+            {
+                CorrectlyFormatted = false;
+                error = "Password is too short, please try again";
+            }
+            else
+            {
+                error = string.Empty;
+                CorrectlyFormatted = true;
+            }
+            return CorrectlyFormatted;
+        }
+
+
+       
+        #endregion
+
+
+
+    }
+}
